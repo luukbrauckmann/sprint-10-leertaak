167dcaa0852e12b124c683a2c30ca3c6a5776a5a
tom-2810 <tom.poortman@hva.nl>
2023-05-09 13:09:20 +0200

    project overview and filter setup

---
+++ public/scripts/new-project-dialog.js
@@ -0,0 +1,19 @@
+const body = document.body;
+
+const newProjectDialog = document.querySelector("#new-project-dialog");
+const showDialogButton = document.querySelector("#new-project-button");
+const closeDialog = newProjectDialog.querySelector("#close");
+
+const formHTML = document.querySelector(".filter-section form");
+
+formHTML.hidden = true;
+newProjectDialog.hidden = false;
+showDialogButton.hidden = false;
+
+showDialogButton.addEventListener("click", () => {
+    newProjectDialog.showModal();
+});
+
+closeDialog.addEventListener("click", () => {
+    newProjectDialog.close();
+})
--- routes/partners.js
+++ routes/partners.js
@@ -7,7 +7,7 @@ const options = {
 	path: '/partners',
 	title: 'partners',
 	template: './partners.ejs',
-	styles: '/styles/start.css'
+	styles: 'partners.css'
 }
 
 
@@ -16,8 +16,40 @@ partners.get('/partners', async (request, response) => {
 	const websites = await get('websites', query)
 		.then((res) => res.websites)
 
-	
-	response.render('index', { ...options, websites })
+	const projects = await get('urls', query)
+		.then((res) => res.urls)
+
+	// Route voor projects
+	/**
+	 * @author Luuk Brauckmann
+	 */
+	// defines different sorts as JSON objects within an Array
+	const sortOptions = [
+		{ label: 'Recent', field: 'createdAt', direction: 'DESC' },
+		{ label: 'Partner A-Z', field: 'titel', direction: 'ASC' },
+		{ label: 'Partner Z-A', field: 'titel', direction: 'DESC' }
+	]
+
+	// gets sort value from url query param
+	let { sort } = request.query
+	// parses the value so it can be accessed as JSON
+	if (sort) sort = JSON.parse(sort)
+	// url doesn't contain a sort option, chooses default sort option
+	else sort = sortOptions[0]
+
+	// clones allProjectsData.urls so .reverse() function won't affect the original
+	let clonedProjects = [...projects];
+	// gets the underlaying website titel and maps it straight to url as titel
+	clonedProjects = clonedProjects.map((url) => ({ ...url, titel: url.website.titel }))
+
+	// sorts every url by given sort option field of cloned allProjectsData.urls
+	clonedProjects = clonedProjects.sort((a, b) => a[sort.field].localeCompare(b[sort.field]))
+	//checks for sorting direction, if direction is DESC, reverse cloned urls ones
+	if (sort.direction === 'DESC') clonedProjects.reverse()
+
+	console.log(clonedProjects)
+
+	response.render('index', { ...options, websites, sortOptions, currentSort: sort, projects: clonedProjects })
 })
 
 export default partners
